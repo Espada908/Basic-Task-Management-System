0b4c481e82772a52e4dddfbfd521b9fd285e8a7b
Espada908 <sorianopauleduard272004@gmail.com>
2025-01-09 21:44:07 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
+require('dotenv').config();
 const express = require('express');
-const dotenv = require('dotenv');
 const taskRoutes = require('./routes/taskRoutes');
-
-dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 
 app.use(express.json());
 
 
-app.use('/api', taskRoutes);
-
+app.use('/tasks', taskRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
